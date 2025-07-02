97f020db158fcc29883465198254c703c0aedcb2
Punith1117 <punith11172006@gmail.com>
2025-07-02 19:48:04 +0530

    add queries to login and signup user using username and password

--- src/apiQueries.js
+++ src/apiQueries.js
@@ -17,3 +17,41 @@ export const getPostCommentsQuery = async (id) => {
         .then(res => res.json())
     return comments
 }
+
+export const loginQuery = async (username, password) => {
+    return await fetch(`${URL}/auth/login`, {
+        method: 'POST',
+        headers:  {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then(async res => {
+        if (!res.ok) {
+            res = await res.json()
+            throw new Error(res.message)
+        }
+        return res.json()
+    })
+}
+
+export const signupQuery = async (username, password) => {
+    await fetch(`${URL}/auth/signup`, {
+        method: 'POST',
+        headers:  {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then(async res => {
+        if (!res.ok) {
+            res = await res.json()
+            throw new Error(res.message)
+        }
+        return await res.json()
+    })
+}
