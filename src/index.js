6bb88fc6cceabcd611192f7a0526605a75e19d23
Punith1117 <punith11172006@gmail.com>
2025-07-02 14:42:51 +0530

    add initial redirection to posts and event listeners to navigation buttons
    The event listener callbacks redirect to respective pages on click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import { redirect } from "./redirect";
 import "./styles/reset.css";
+
+(async () => await redirect('posts'))()
+
+document.querySelector('.posts-button')
+    .addEventListener('click', () => redirect('posts'))
+document.querySelector('.my-comments-button')
+    .addEventListener('click', () => redirect('my-comments'))
