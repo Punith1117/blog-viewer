f15097412dc944c4f61f3397f359dcd1d908f9d5
Punith1117 <punith11172006@gmail.com>
2025-07-03 13:16:38 +0530

    add redirect logic to login and signup pages and modify view-post page

--- src/redirect.js
+++ src/redirect.js
@@ -3,6 +3,8 @@ import { allPosts } from "./components/posts"
 import htmlTruncate from 'html-truncate'
 import { viewPost } from "./components/view-post"
 import { postComments } from "./components/post-comments"
+import { signupLogin } from "./components/signup-login"
+import { destroyJwt, destroyUsername, getJwt } from "./utilities"
 
 export const redirect = async (page, data) => {
     const main = document.querySelector('main')
@@ -18,9 +20,22 @@ export const redirect = async (page, data) => {
             main.replaceChildren(allPosts(posts))
             break
         case 'view-post':
-            let post = await getPostQuery(data.id)
-            let comments = await getPostCommentsQuery(data.id)
-            main.replaceChildren(viewPost(post), postComments(comments))
+            let post = await getPostQuery(data.postId)
+            let comments = await getPostCommentsQuery(data.postId)
+            main.replaceChildren(viewPost(post), postComments(comments, {
+                postId: data.postId,
+                isAuthenticated: (getJwt() === null) ? false : true
+            }))
+            break
+        case 'login':
+            destroyJwt()
+            destroyUsername()
+            main.replaceChildren(signupLogin('login', data))
+            break
+        case 'signup':
+            destroyJwt()
+            destroyUsername()
+            main.replaceChildren(signupLogin('signup', data))
             break
         default:
             main.textContent = 'This page does not exit'
