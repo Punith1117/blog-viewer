d31a403e2deb18b4add26eed78ca8738184171e3
Punith1117 <punith11172006@gmail.com>
2025-07-03 19:05:02 +0530

    add deployed api url

--- src/apiQueries.js
+++ src/apiQueries.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:3000'
+const URL = 'https://blog-api-p64x.onrender.com'
 
 export const getAllPostsQuery = async () => {
     const { posts } = await fetch(`${URL}/post`)
